3bffef490bfdaf323693b21930e0ac2ea598c31a
RaphaelMa <raphaelmariano197@gmail.com>
2022-04-08 01:13:40 -0300

    feature: create infra in project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-canil
+Projeto node
+
+### Pré-requisitos globais:
+`npm -i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install ou yarn`
+
+
+### Para rodar o projeto
+`npm run dev ou yarn dev`s
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes';
+
+dotenv.config();
+
+const app = express();
+
+// Config template enginey
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('mustache', mustache());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+//Routes
+app.use(mainRoutes);
+
+app.use((req, res) => {
+  res.send('Pagina não encontrada!');
+})
+
+app.listen(process.env.PORT || 3001);
+
